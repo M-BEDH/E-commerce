35aef44cfd3b8c021391210f375c590642fb989e
M-BEDH <mel.b.ms@pm.me>
2025-08-06 15:15:58 +0200

    Ajout City et Order

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'app_order')]
+    public function index(Request $request): Response
+    {
+
+        $order = new Order();
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        return $this->render('order/index.html.twig', [
+           'form'=>$form->createView()
+        ]);
+    }
+}
