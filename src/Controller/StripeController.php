79cb1d4c77a0b2c6b5dc40128391bac113e2a1a9
M-BEDH <mel.b.ms@pm.me>
2025-08-19 16:11:50 +0200

    edit - use inutilisé

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -6,7 +6,6 @@ use Stripe\Stripe;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Stripe\Exception\SignatureVerifcationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class StripeController extends AbstractController
